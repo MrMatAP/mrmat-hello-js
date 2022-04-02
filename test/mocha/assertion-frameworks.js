66edf433a7000a5c65bade7705532cd848bf8f96
Mathieu Imfeld <imfeldma@gmail.com>
2022-04-02 11:44:40 +0200

    Added test for assertion frameworks

---
+++ test/mocha/assertion-frameworks.js
@@ -0,0 +1,76 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022  Mr Mat
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const assert = require('assert');
+require('should')
+const expect = require('expect.js')
+const chai_expect = require('chai').expect
+const chai_assert = require('chai').assert
+const better_assert = require('better-assert')
+const expected = require('unexpected')
+
+
+describe('Mocha Basics', function() {
+
+    /**
+     * There are various means to use assertions in Mocha
+     */
+    describe('Mocha Assertion Frameworks', function() {
+
+        it('Node Assert', function() {
+            assert.equal([1,2,3].indexOf(4), -1)
+        })
+
+        it('Should', function() {
+            ([1,2,3].indexOf(4)).should.be.exactly(-1)
+        })
+
+        it('Expect', function() {
+            expect([1,2,3].indexOf(4)).to.equal(-1)
+        })
+
+        // We cannot test Chain - Should because both should.js and Chai would modify
+        // the Object prototype
+
+        it('Chai - Expect', function() {
+            chai_expect([1,2,3].indexOf(4)).to.equal(-1)
+        })
+
+        it('Chai - Assert', function() {
+            chai_assert.equal(
+                [1,2,3].indexOf(4),
+                -1,
+                'The indexOf function returns -1 for out of bounds indices')
+        })
+
+        it('Better Assert', function() {
+            better_assert([1,2,3].indexOf(4) === -1)
+        })
+
+        it('Unexpected', function() {
+            expected([1,2,3].indexOf(4), 'to be', -1)
+        })
+    })
+
+})
